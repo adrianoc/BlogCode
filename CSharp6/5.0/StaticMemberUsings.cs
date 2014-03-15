6d1097e579f5ea5df942ab5b6c38c0eea8c723d5
Adriano Carlos Verona <adriano.verona@gmail.com>
2014-03-15 13:52:20 -0300

    static member usings

---
+++ CSharp6/5.0/StaticMemberUsings.cs
@@ -0,0 +1,7 @@
+class Exemplo
+{
+	public double M()
+	{
+		return System.Math.Sqrt(1.0, 4.2);
+	}
+}
---
+++ CSharp6/6.0/StaticMemberUsings.cs
@@ -0,0 +1,9 @@
+using System.Math;
+
+class Exemplo
+{
+	public double M()
+	{
+		return Sqrt(1.0, 4.2);
+	}
+}

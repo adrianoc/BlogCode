2fad34084872a4525f9d4098c2101f10d2862537
Adriano Carlos Verona <adriano.verona@gmail.com>
2014-04-05 17:57:11 -0300

    collection initializer

---
+++ CSharp6/5.0/CollectionInitializer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+
+class Coll : IEnumerable
+{
+	public void Add(string s)
+	{
+		System.Console.WriteLine(s);
+	}
+
+	public IEnumerator GetEnumerator() { return null; }
+}
+
+class Test
+{
+	public static void Main(string[] args)
+	{
+		var x = new Coll() { "foo", "bar", "baz" };
+	}
+}

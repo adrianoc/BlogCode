accb93fd55ef08424e5c08d98bfd185e3ced03c2
Adriano Carlos Verona <adriano.verona@gmail.com>
2014-04-13 10:51:40 -0300

    Primary ctors

--- CSharp6/6.0/PrimaryConstructors.cs
+++ CSharp6/6.0/PrimaryConstructors.cs
@@ -1,5 +1,14 @@
-class Exemplo (int valor, string nome)
+class Example (private int value, string n)
 {
-	private int valor;
-	private string nome;
+	private string name = n;
+
+	public static void Main(string[] args)
+	{
+		new Example(42, "foo").Print();
+	}
+
+	private void Print()
+	{
+		System.Console.WriteLine("{0} = {1}", name, value);
+	}
 }

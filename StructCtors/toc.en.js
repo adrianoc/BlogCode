1ca8ca041dff275f4c726ba6735c8131beddccbb
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-01-29 08:11:11 -0500

    update en/pt tocs

--- StructCtors/toc.en.js
+++ StructCtors/toc.en.js
@@ -5,8 +5,8 @@
         { "description": "Brief introduction to Value Types vs Reference Types", "url": "https://programing-fun.blogspot.com/2023/07/structs-in-c-are-fun-part-29-brief.html" },
         { "description": "Field initialization in structs", "url": "https://programing-fun.blogspot.com/2023/08/structs-in-c-are-fun-part-39-field.html" },
         { "description": "Constructors and struct behavior", "url": "https://programing-fun.blogspot.com/2023/11/structs-in-c-are-fun-part-49.html" },
-        { "description": "Other scenarios in which struct constructors behavior may surprise you", "url": "https://programing-fun.blogspot.com/2023/12/structs-in-c-are-fun-part-59-other.html." },
-        { "description": "Struct with default argument values in constructors, a.k.a, are you not confused yet?", "url": "https://programing-fun.blogspot.com/2024/01/structs-in-c-are-fun-part-69-struct.html." },
+        { "description": "Other scenarios in which struct constructors behavior may surprise you", "url": "https://programing-fun.blogspot.com/2023/12/structs-in-c-are-fun-part-59-other.html" },
+        { "description": "Struct with default argument values in constructors, a.k.a, are you not confused yet?", "url": "https://programing-fun.blogspot.com/2024/01/structs-in-c-are-fun-part-69-struct.html" },
         { "description": "<a href='https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/required'>required</a> feature from C# 11 will not save your <del>a**</del> job." },
         { "description": "Struct used as default argument values." },
         { "description": "Conclusion." }

9755dc41a2dba75620fabcb3aaeea5cc358eeff4
Adriano Carlos Verona <adriano.verona@gmail.com>
2025-01-29 07:59:30 -0500

    update portuguese toc

--- StructCtors/toc.pt.js
+++ StructCtors/toc.pt.js
@@ -5,7 +5,7 @@
        { "description": "Breve introdução a Tipos de Valor vs Tipos de Referência", "url": "https://programing-fun.blogspot.com/2023/07/structs-in-c-are-fun-part-29-brief.html" },
        { "description": "Inicialização de campos em structs", "url": "https://programing-fun.blogspot.com/2023/08/structs-in-c-are-fun-part-39-field.html" },
        { "description": "Construtores e comportamento das structs", "url": "https://programing-fun.blogspot.com/2023/11/structs-in-c-are-fun-part-49.html" },
-       { "description": "Outros cenários nos quais o comportamento dos construtores de structs pode te surpreender", "url": "https://programing-fun.blogspot.com/2023/12/structs-in-c-are-fun-part-59-other.html" },
+       { "description": "Outros cenários nos quais o comportamento dos construtores de structs pode te surpreender", "url": "https://programing-fun.blogspot.com/2023/12/structs-em-c-diversao-garantida-parte.html" },
        { "description": "Struct com valores padrão para argumentos nos construtores, ou seja, você não está confuso ainda?", "url": "https://programing-fun.blogspot.com/2024/01/structs-in-c-are-fun-part-69-struct.html" },
        { "description": "A funcionalidade <a href='https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/required'>required</a> do C# 11 não vai salvar seu <del>c**</del> emprego." },
        { "description": "Struct usada como valores padrão para argumentos." },
